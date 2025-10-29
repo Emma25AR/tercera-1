a8cfa55d04ce6b088eae7b013052604291714cce
Emanuel Arias <emanuelam2583@gmail.com>
2025-10-29 14:00:49 -0600

    Footer

--- vistas/vista_inferior.php
+++ vistas/vista_inferior.php
@@ -1,11 +1,44 @@
 
+<footer class="main-footer">
+  <div class="footer-container">
 
+    <div class="footer-logo">
+      <img src="./imgs/logoG.png" alt="Logo" height="60">
+      <p>Tienda Musical 🎸</p>
+    </div>
 
+    <div class="footer-links">
+      <h4>Enlaces</h4>
+      <ul>
+        <li><a href="./">Inicio</a></li>
+        <li><a href="./guitarra.php">Guitarras</a></li>
+        <li><a href="./bajos.php">Bajos</a></li>
+        <li><a href="./baterias.php">Baterías</a></li>
+        <li><a href="./contacto.php">Contacto</a></li>
+      </ul>
+    </div>
 
+    <div class="footer-social">
+      <h4>Síguenos</h4>
+      <div class="social-icons">
+        <a href="https://www.facebook.com/"><img src="./imgs/icon-facebook.png" alt="Facebook"></a>
+        <a href="https://www.instagram.com"><img src="./imgs/icon-instagram.png" alt="Instagram"></a>
+        <a href="https://www.youtube.com"><img src="./imgs/icon-youtube.png" alt="YouTube"></a>
+      </div>
+    </div>
 
-</html>
+  </div>
+
+  <div class="footer-bottom">
+    <p>© 2025 Tienda Musical - Todos los derechos reservados</p>
+  </div>
+</footer>
+
 
 <script src="./script/hamburger-icon.js"></script>
 <script src="./script/cards.js"></script>
 <script src="./script/accordion.js"></script>
 <script src="./script/carousel.js"></script> 
+
+</html>
+
